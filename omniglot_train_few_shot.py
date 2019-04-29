da3d1650b0f05e098b2e79de401a3e7db761801c
Arbitersc <keporisgreat@126.com>
2019-04-29 22:35:59 +0800

    omnigit

--- omniglot_train_few_shot.py
+++ omniglot_train_few_shot.py
@@ -79,3 +79,26 @@ class RelationNetwork(nn.Module):
                         nn.MaxPool2d(2))
         self.fc1 = nn.Linear(input_size, hidden_size)
         self.fc2 = nn.Linear(hidden_size, 1)
+
+    def forward(self, x):
+        out = self.layer1(x)
+        out = self.layer2(out)
+        out = out.view(out.size(0), -1)
+        out = F.relu(self.fc1(out))
+        out = F.sigmoid(self.fc2(out))
+        return out
+
+def weights_init(m):
+    classname = m.__class__.__name__
+    if classname.find('Conv') != -1:
+        n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+        m.weight.data.normal_(0, math.sqrt(2. / n))
+        if m.bias is not None:
+            m.bias.data.zero_()
+    elif classname.find('BatchNorm') != -1:
+        m.weight.data.fill(1)
+        m.bias.data.zero_()
+    elif classname.find('Linear') != -1:
+        n = m.weight.size(1)
+        m.weight.data.normal_(0, 0.01)
+        m.bias.data = torch.ones(m.bias.data.size())
--- task_generator.py
+++ task_generator.py
@@ -1,1 +1,53 @@
- 
+import torchvision
+import torchvision.datasets as dset
+import torchvision.transforms as transforms 
+import torch
+from torch.utils.data import DataLoader, Dataset
+import random
+import os
+from PIL import Image
+import matplotlib.pyplot as plt
+import numpy as np
+from torch.utils.data.sampler import Sampler
+
+def imshow(img):
+    npimg = img.numpy()
+    plt.axis("off")
+    plt.imshow(np.transpose(npimg, (1,2,0)))
+    plt.show()
+
+class Rotate(object):
+    def __init__(self. angle):
+        self.angle = angle
+    
+    def __call__(self, x, mode="reflect"):
+        x = x.rotate(self.angle)
+        return x
+
+def ominiglot_character_folders():
+    data_folder = '../datas/omniglot_resized/'
+
+    character_folders = [os.path.join(data_folder, family, charater) \ 
+                for family in os.listdir(data_folder) \
+                if os.path.isdir(os.path.join(data_folder, family)) \
+                for character in os.listdir(os.path.join(data_folder, family))]
+    random.seed(1)
+    random.shuffle(character_folders)
+
+    num_train = 1200
+    metatrain_character_folder = character_folders[:num_train]
+    metaval_character_folder = character_folders[num_train:]
+    
+    return metatrain_character_folder, metaval_character_folder
+
+class OmniglotTask(object):
+    def __init__(self, character_folders, num_classes, train_num, test_num):
+        self.character_folders = character_folders
+        self.num_classes = num_classes
+        self.train_num = train_num
+        self.test_num = test_num
+
+        class_folders = random.sample(self.character_folders, self.num_classes)
+        labels = np.array(range(len(class_folders)))
+        labels = dict(zip(class_folders, labels))
+        samples = dict()
