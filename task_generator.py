0d7d7e01567a76b25faef48ea8fe67baf7388794
Arbitersc <keporisgreat@126.com>
2019-05-08 16:02:00 +0800

    omnigit

--- task_generator.py
+++ task_generator.py
@@ -17,7 +17,8 @@ def imshow(img):
     plt.show()
 
 class Rotate(object):
-    def __init__(self. angle):
+    
+    def __init__(self, angle):
         self.angle = angle
     
     def __call__(self, x, mode="reflect"):
@@ -27,7 +28,7 @@ class Rotate(object):
 def ominiglot_character_folders():
     data_folder = '../datas/omniglot_resized/'
 
-    character_folders = [os.path.join(data_folder, family, charater) \ 
+    character_folders = [os.path.join(data_folder, family, character) \
                 for family in os.listdir(data_folder) \
                 if os.path.isdir(os.path.join(data_folder, family)) \
                 for character in os.listdir(os.path.join(data_folder, family))]
@@ -47,7 +48,57 @@ class OmniglotTask(object):
         self.train_num = train_num
         self.test_num = test_num
 
+        # 随机取样类别
         class_folders = random.sample(self.character_folders, self.num_classes)
         labels = np.array(range(len(class_folders)))
         labels = dict(zip(class_folders, labels))
         samples = dict()
+
+        self.train_roots = []
+        self.test_roots = []
+        for c in class_folders:
+
+            # 随机取样类别中的样本
+            temp = [os.path.join(c, x) for x in os.listdir(c)]
+            samples[c] = random.sample(temp, len(temp))
+
+            self.train_roots += samples[c][:train_num]
+            self.test_roots += samples[c][train_num:train_num + test_num]
+
+        self.train_labels = [labels[self.get_class(x)] for x in self.train_roots]
+        self.test_labels = [labels[self.get_class(x)] for x in self.test_roots]
+
+    def get_class(self, sample):
+        return os.path.join(*sample.split('/'[:-1]))
+
+class FewShotDataset(Dataset):
+
+     def __init__(self, task, split='train', transform=None, target_transform=None):
+        self.transform = transform
+        self.target_transform = target_transform
+        self.task = task
+        self.split = split
+        self.image_roots = self.task.train_roots if self.split == 'train' else self.task.test_roots
+        self.labels = self.task.train_labels if self.split == 'train' else self.task.test_labels
+
+    def __len__(self):
+        return len(self.image_roots)
+    
+    def __getitem__(self, idx):
+        raise NotImplementedError("This is an abstract class. Subclass this class for your particular dataset.")
+
+class Omniglot(FewShotDataset):
+    def __init__(self, *args. **kwargs):
+        super(Omniglot, self).__init__(*args, **kwargs)
+
+    def __getitem__(self, idx):
+        image_root = self.image_roots[idx]
+        image = Image.open(image_root)
+        image = image.convert('L')
+        image = image.resize((28, 28), resample=Image.LANCZOS)
+        if self.transform is not None:
+            image = self.transform(image)
+        label = self.labels[idx]
+        if self.target_transform is not None:
+            label = self.target_transform(label)
+        return image, label
